faae957144aee7b23ae0120afb2bc0e6437269ac
Cameron Manderson <cameronmanderson@gmail.com>
2011-07-25 11:07:35 +1000

    Updated paths to new folders

--- WebRoot/index.php
+++ WebRoot/index.php
@@ -1,19 +1,19 @@
 <?php
 // Include Aloi
-require_once('../lib/aloi/Aloi.php');
+require_once('../vendors/aloi/Aloi.php');
 
 // Register the autoloader and set include paths
-Aloi::registerAutoload(array('../src', '../lib', '../lib/aloi'));
+Aloi::registerAutoload(array('../src', '../vendors', '../vendors/aloi'));
 
 // Log4PHP - Disable in production for speed
 $logging = true;
 if($logging) {
-	define('LOG4PHP_CONFIGURATION', 'WEB-INF/log4php.properties');
+	define('LOG4PHP_CONFIGURATION', '../app/log4php.properties');
 	require_once('log4php/LoggerManager.php');
 	Aloi_Util_Logger_Manager::setLoggerManagerClass('LoggerManager');
 }
 
 // Handle the request
-Aloi_Serphlet_Host::addConfigurationFile('../conf/web.xml');
-Aloi_Serphlet_Host::setCacheDirectory('../conf/cache');
+Aloi_Serphlet_Host::addConfigurationFile('../app/web.xml');
+Aloi_Serphlet_Host::setCacheDirectory('../app/cache');
 Aloi_Serphlet_Host::process(dirname(__FILE__));
