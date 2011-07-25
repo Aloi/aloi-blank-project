42ae6f59e9736f29c465afac488d0b68c5b2383b
Cameron Manderson <cameronmanderson@gmail.com>
2011-07-25 14:47:26 +1000

    Updated example for running the hello world

--- app/autoload.php
+++ app/autoload.php
@@ -1,13 +1,21 @@
-// Include Aloi
-require_once('../vendors/aloi/Aloi.php');
+<?php
+// Require the class loader
+require(__DIR__ . '/../vendor/aloi/Aloi/ClassLoader.php');
 
-// Register the autoloader and set include paths
-Aloi::registerAutoload(array('../src', '../vendors', '../vendors/aloi'));
+// Register the prefixes to paths
+$loader = new Aloi_ClassLoader();
+$loader->registerPrefixes(array(
+    'Aloi_Deployment' => __DIR__ . '/../vendor/deployment',
+    'Aloi_Routes' => __DIR__ . '/../vendor/httpserver',
+    'Aloi_Serphlet' => __DIR__ . '/../vendor/httpserver',
+    'Aloi_Phigester' => __DIR__ . '/../vendor/phigester',
+    'Aloi_Phruts_Compat' => __DIR__ . '/../vendor/phpmvc-compat',
+    'Aloi_Phruts' => __DIR__ . '/../vendor/phruts',
+    'Aloi_' => __DIR__ . '/../vendor/aloi',
+));
+$loader->registerFallbackIncludePaths(array(
+    '../src',
+    '../vendor'
+));
 
-// Log4PHP - Disable in production for speed
-$logging = true;
-if($logging) {
-	define('LOG4PHP_CONFIGURATION', '../app/conf/log4php.properties');
-	require_once('log4php/LoggerManager.php');
-	Aloi_Util_Logger_Manager::setLoggerManagerClass('LoggerManager');
-}
+$loader->register();
